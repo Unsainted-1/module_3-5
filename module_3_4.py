ff22467134c3a2408a368bc904444e87ef80f1c8
Unsainted-1 <vileydzer@gmail.com>
2024-11-28 13:53:23 +0300

    Add files via upload

---
+++ module_3_4.py
@@ -0,0 +1,10 @@
+def single_root_words(root_word, *other_words):
+    same_words = []
+    for i in range(len(other_words)):
+        if root_word.lower() in other_words[i].lower() or other_words[i].lower() in root_word.lower():
+            same_words.append(other_words[i])
+    return same_words
+
+print(single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies'))
+print(single_root_words('Disablement', 'Able', 'Mable', 'Disable', 'Bagel'))
+print(single_root_words('Дон', 'ЭронДонДон', 'он', 'небонбон', 'бунбун', 'рондононить'))
