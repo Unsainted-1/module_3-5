745ffa0c46d6123206737d34e70d2625102b97d0
Unsainted-1 <vileydzer@gmail.com>
2024-12-06 17:24:21 +0300

    Add files via upload

---
+++ module_4_1.py
@@ -0,0 +1,8 @@
+from fake_math import divide as fake_div
+from true_math import divide as true_div
+
+print(fake_div(52, 2))
+print(fake_div(52, 0))
+print(true_div(77, 7))
+print(true_div(77, 0))
+
